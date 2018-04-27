d86c031d72e90426560b0ebca77048af7cdf8bee
sophierobinson <38134171+sophierobinson@users.noreply.github.com>
2018-04-27 10:59:13 +0100

    Update materninfcovfunction.R

--- materninfcovfunction.R
+++ materninfcovfunction.R
@@ -1,3 +1,23 @@
+#The following code was used to produce Figure 2.3 d)
+
+
+#The GP regression section follows the code produced by James Keirstead: "Gaussian process regression with R" April 5, 2012.
+#The code can be sourced here https://www.r-bloggers.com/gaussian-process-regression-with-r/
+
+#This code uses the package "GPfit" written by Blake MacDonald and Pritam Ranjan and Hugh Chipman, published in 2015.
+#The url is https://cran.r-project.org/web/packages/GPfit/index.html
+#This code uses the package "MASS" written by Brian Ripley et. al., published in 2016.
+#The url is https://cran.r-project.org/web/packages/MASS/MASS.pdf
+#This code uses the package "gridExtra" written by Baptiste Augie, published in 2017.
+#The url is https://cran.r-project.org/web/packages/gridExtra/index.html
+#This code uses the package "plyr" written by Hadley Wickham, published in 2016.
+#The url for this package is https://cran.r-project.org/web/packages/plyr/plyr.pdf
+#This code uses the package "reshape2" written by Hadley Wickham, published in 2017.
+#The url for this package is https://cran.r-project.org/web/packages/reshape2/reshape2.pdf
+#This code uses the package "ggplot2" written by Haldey Wickham and Winston Chang, published in 2016.
+#The url for this package is https://cran.r-project.org/web/packages/ggplot2/ggplot2.pdf
+
+
 library(GPfit)
 require(MASS)
 require(gridExtra)
